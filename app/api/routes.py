1adb44bf64ffe837802c0ff7fbab875613320bf5
Harsh Srivastava <harshjio22@gmail.com>
2025-08-06 13:06:43 +0530

    added api-routes and dependencies

--- app/api/routes.py
+++ app/api/routes.py
@@ -1,1 +1,7 @@
-# Combine all routers here
+from fastapi import APIRouter
+from app.modules.users.api import router as user_router
+from app.modules.notes.api import router as notes_router
+
+api_router = APIRouter()
+api_router.include_router(user_router, prefix="/users", tags=["Users"])
+api_router.include_router(notes_router, prefix="/notes", tags=["Notes"])
